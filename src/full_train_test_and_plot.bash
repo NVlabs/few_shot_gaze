8b75ff5742c7cff3cb1627026114a01b540c2946
Shalini De Mello <shalinig@nvidia.com>
2020-09-13 16:32:26 -0700

    Fixed training flags and paths to .h5 files.

--- src/full_train_test_and_plot.bash
+++ src/full_train_test_and_plot.bash
@@ -11,13 +11,11 @@
 #       `1_train_dt_ed.py` in that it skips the script completely.
 #
 # Set to 0 to perform DT-ED training and inference for the HDF output.
-SKIP_DTED_TRAINING=0
+SKIP_DTED_TRAINING=1
 
 # NOTE: please make sure to update the two paths below as necessary.
-#MPIIGAZE_FILE="../preprocess/outputs/MPIIGaze.h5"
-#GAZECAPTURE_FILE="../preprocess/outputs/GazeCapture.h5"
-MPIIGAZE_FILE="../../few_shot_gaze_internal/data/47946/MPIIGaze.h5"
-GAZECAPTURE_FILE="../../few_shot_gaze_internal/data/47946/GazeCapture.h5"
+MPIIGAZE_FILE="../preprocess/outputs/MPIIGaze.h5"
+GAZECAPTURE_FILE="../preprocess/outputs/GazeCapture.h5"
 
 # This batch size should fit a 11GB single GPU
 # The original training used 8x Tesla V100 GPUs.
